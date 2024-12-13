68e9f1eac627b6b8662959b619712606042eb034
MassimoSporchia <massimo.sporchia@datadoghq.com>
2024-12-13 15:08:52 +0100

    Initial commit

--- complete/src/main/java/com/example/springboot/HelloController.java
+++ complete/src/main/java/com/example/springboot/HelloController.java
@@ -1,14 +1,19 @@
 package com.example.springboot;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
-	@GetMapping("/")
-	public String index() {
-		return "Greetings from Spring Boot!";
-	}
+    private static final Logger logger = LoggerFactory.getLogger(HelloController.class);
+
+    @GetMapping("/")
+    public String index() {
+        logger.info("Handling root mapping request");
+        return "Greetings from Spring Boot!";
+    }
 
 }
