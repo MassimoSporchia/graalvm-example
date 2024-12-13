bbb959d43188c273ee4b09e4643402d085ffd0a1
MassimoSporchia <massimo.sporchia@datadoghq.com>
2024-12-13 18:29:25 +0100

    added kafka

--- complete/src/main/java/com/example/springboot/Application.java
+++ complete/src/main/java/com/example/springboot/Application.java
@@ -1,33 +1,80 @@
 package com.example.springboot;
 
-import java.util.Arrays;
+import org.springframework.boot.ApplicationRunner;
 
-import org.springframework.boot.CommandLineRunner;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaOperations;
+import org.springframework.kafka.listener.CommonErrorHandler;
+import org.springframework.kafka.listener.DeadLetterPublishingRecoverer;
+import org.springframework.kafka.listener.DefaultErrorHandler;
+import org.springframework.kafka.support.converter.JsonMessageConverter;
+import org.springframework.kafka.support.converter.RecordMessageConverter;
+import org.springframework.util.backoff.FixedBackOff;
 
 @SpringBootApplication
 public class Application {
+
+	private static final Logger logger = LoggerFactory.getLogger(HelloController.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 
+	/*
+	 * Boot will autowire this into the container factory.
+	 */
 	@Bean
-	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
-		return args -> {
-
-			System.out.println("Let's inspect the beans provided by Spring Boot:");
-
-			String[] beanNames = ctx.getBeanDefinitionNames();
-			Arrays.sort(beanNames);
-			for (String beanName : beanNames) {
-				System.out.println(beanName);
-			}
-
-		};
+	public CommonErrorHandler errorHandler(KafkaOperations<Object, Object> template) {
+		return new DefaultErrorHandler(
+				new DeadLetterPublishingRecoverer(template), new FixedBackOff(1000L, 2));
 	}
 
+	@Bean
+	public RecordMessageConverter converter() {
+		return new JsonMessageConverter();
+	}
+
+	@KafkaListener(id = "fooGroup", topics = "topic1")
+	public void listen(String foo) {
+		logger.info("Received: " + foo);
+		// if (foo.getFoo().startsWith("fail")) {
+		// 	throw new RuntimeException("failed");
+		// }
+		// this.exec.execute(() -> System.out.println("Hit Enter to terminate..."));
+	}
+
+	@KafkaListener(id = "dltGroup", topics = "topic1.DLT")
+	public void dltListen(byte[] in) {
+		logger.info("Received from DLT: " + new String(in));
+		// this.exec.execute(() -> System.out.println("Hit Enter to terminate..."));
+	}
+
+	@Bean
+	public NewTopic topic() {
+		return new NewTopic("topic1", 1, (short) 1);
+	}
+
+	@Bean
+	public NewTopic dlt() {
+		return new NewTopic("topic1.DLT", 1, (short) 1);
+	}
+
+	@Bean
+	@Profile("default") // Don't run from test(s)
+	public ApplicationRunner runner() {
+		return args -> {
+			System.out.println("Hit Enter to terminate...");
+			System.in.read();
+		};
+	}
 }
+
